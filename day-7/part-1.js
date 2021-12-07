81031d1d36dd8b0731315691684586f28a14468a
Sebastian Ammon <seb.ammon@gmail.com>
2021-12-07 07:21:58 +0100

    Update fuel cost formula and some reformatting

--- day-7/part-1.js
+++ day-7/part-1.js
@@ -3,8 +3,6 @@ const {readFile, median} = require("../utils");
 const numbers = readFile().filter(Boolean)[0].split(',').map(Number);
 
 const medianValue = median(numbers);
-
-const fuel = numbers.map(num => Math.abs(medianValue - num))
-const fuelCost = fuel.reduce((s, c) => s + c, 0)
+const fuelCost = numbers.map(num => Math.abs(medianValue - num)).reduce((s, c) => s + c, 0)
 
 console.log(`Target position: ${medianValue} with fuel cost: ${fuelCost}`)
--- day-7/part-2.js
+++ day-7/part-2.js
@@ -1,28 +1,13 @@
-const {readFile, sort} = require("../utils");
+const {readFile} = require("../utils");
 
-const positions = sort(readFile().filter(Boolean)[0].split(',').map(Number));
+const positions = readFile().filter(Boolean)[0].split(',').map(Number);
 
-function getFuelCost(diff) {
-    if (diff === 0) {
-        return 0
-    }
+const getFuelCost = diff => diff * (diff + 1) / 2;
+const getTotalFuelCost = (positions, position) => positions.map(v => getFuelCost(Math.abs(v - position))).reduce((s, c) => s + c, 0);
 
-    let cost = 1;
-    while (diff !== 1) {
-        cost += diff
-        diff--
-    }
-    return cost
-}
-
-function getTotalFuelCost(positions, position) {
-    return positions.map(v => getFuelCost(Math.abs(v - position))).reduce((s, c) => s + c, 0)
-}
-
-
-let targetPosition = positions[positions.length - 1];
+let targetPosition = Math.max(...positions);
 let minimumFuelCost = getTotalFuelCost(positions, targetPosition);
-for (let position = positions[positions.length - 1]; position >= 0; position--) {
+for (let position = targetPosition; position >= 0; position--) {
     const currentFuelCost = getTotalFuelCost(positions, position);
 
     if (currentFuelCost < minimumFuelCost) {
